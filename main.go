afff44b3cce92ec2b9dc41c06796ab530b30e2a9
fiqih16 <fiqih1666@gmail.com>
2022-08-14 20:15:19 +0700

    Logout

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func main() {
 
 	r.HandleFunc("/login", authcontroller.Login).Methods("POST")
 	r.HandleFunc("/register", authcontroller.Register).Methods("POST")
-	r.HandleFunc("/logout", authcontroller.Logout).Methods("POST")
+	r.HandleFunc("/logout", authcontroller.Logout).Methods("GET")
 
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
