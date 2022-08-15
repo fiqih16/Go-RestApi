0684d4d9ca099091acc84f21609be529e5b8814f
fiqih16 <fiqih1666@gmail.com>
2022-08-15 18:26:47 +0700

    Web login & Register

--- controllers/admincontroller/admincontroller.go
+++ controllers/admincontroller/admincontroller.go
@@ -1,1 +1,21 @@
 package admincontroller
+
+import (
+	"html/template"
+	"net/http"
+)
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	temp, _ := template.ParseFiles("views/home/index.html")
+	temp.Execute(w, nil)
+}
+
+func DAadmin(w http.ResponseWriter, r *http.Request) {
+	temp, _ := template.ParseFiles("views/dashboard/dashboard.html")
+	temp.Execute(w, nil)
+}
+
+func Login(w http.ResponseWriter, r *http.Request) {
+	temp, _ := template.ParseFiles("views/login/login.html")
+	temp.Execute(w, nil)
+}
--- main.go
+++ main.go
@@ -1,22 +1,29 @@
 package main
 
 import (
-	"log"
+	"fmt"
+	// "log"
 	"net/http"
 
-	"github.com/fiqih16/Go-RestApi/controllers/authcontroller"
+	admincontroller "github.com/fiqih16/Go-RestApi/controllers/admincontroller"
 	"github.com/fiqih16/Go-RestApi/models"
-
-	"github.com/gorilla/mux"
+	// "github.com/fiqih16/Go-RestApi/controllers/authcontroller"
+	// "github.com/gorilla/mux"
 )
 
 func main() {
 	models.ConnectDatabase()
-	r := mux.NewRouter()
+	// r := mux.NewRouter()
 
-	r.HandleFunc("/login", authcontroller.Login).Methods("POST")
-	r.HandleFunc("/register", authcontroller.Register).Methods("POST")
-	r.HandleFunc("/logout", authcontroller.Logout).Methods("GET")
+	// r.HandleFunc("/login", authcontroller.Login).Methods("POST")
+	// r.HandleFunc("/register", authcontroller.Register).Methods("POST")
+	// r.HandleFunc("/logout", authcontroller.Logout).Methods("GET")
+	
+	// log.Fatal(http.ListenAndServe(":8080", r))
 
-	log.Fatal(http.ListenAndServe(":8080", r))
-}
+	http.HandleFunc("/", admincontroller.Index)
+	http.HandleFunc("/dashboard", admincontroller.DAadmin)
+	http.HandleFunc("/login", admincontroller.Login)
+	http.ListenAndServe(":8080", nil)
+	fmt.Println("Server started on port 8080")
+} // 34:01
